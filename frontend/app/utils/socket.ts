0186fb24edc6852ae8206e1f632e055773a282fb
Jensi Kachhadiya <jensikachhadiya4@gmail.com>
2025-12-16 09:21:05 +0530

    socket url

--- frontend/app/utils/socket.ts
+++ frontend/app/utils/socket.ts
@@ -8,6 +8,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
+export const socket = io("https://realtimechat-h3w4.onrender.com", {
   transports: ["polling", "websocket"],
 });
